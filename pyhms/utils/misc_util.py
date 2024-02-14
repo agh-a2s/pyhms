22d3dc86b65b141cc4949ac045604d405bf784dd
WojtAcht <wachtelik@gmail.com>
2024-02-14 10:56:36 +0100

    feat: move unique_file_name to grid

--- pyhms/utils/misc_util.py
+++ pyhms/utils/misc_util.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import List
 
 import numpy as np
@@ -7,9 +6,3 @@ from leap_ec.individual import Individual
 
 def compute_centroid(population: List[Individual]) -> np.ndarray:
     return np.mean([ind.genome for ind in population], axis=0)
-
-
-def unique_file_name(prefix, ext):
-    dt_now = datetime.now()
-    dt_part = dt_now.strftime("-%Y%m%d-%H%M%S")
-    return prefix + dt_part + ext
