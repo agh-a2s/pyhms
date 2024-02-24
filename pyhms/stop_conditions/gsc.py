3cc80e63aa2f0ef50328ef54d0b7036a04497652
WojtAcht <wachtelik@gmail.com>
2024-02-24 11:45:01 +0100

    feat: improve gsc

--- pyhms/stop_conditions/gsc.py
+++ pyhms/stop_conditions/gsc.py
@@ -1,9 +1,11 @@
 """
     Global stopping conditions.
 """
+
 import logging
 from abc import ABC, abstractmethod
-from typing import Any, List, Union
+from enum import StrEnum
+from typing import Any
 
 from ..problem import EvalCountingProblem, PrecisionCutoffProblem, StatsGatheringProblem
 from ..tree import DemeTree
@@ -11,33 +13,66 @@ from ..tree import DemeTree
 logger = logging.getLogger(__name__)
 
 
-class gsc(ABC):
+class GSC(ABC):
     @abstractmethod
     def satisfied(self, tree: DemeTree) -> bool:
         raise NotImplementedError()
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
+    def __call__(self, *args: Any, **kwds: Any) -> bool:
         return self.satisfied(*args, **kwds)
 
 
-class root_stopped(gsc):
+class RootStopped(GSC):
+    """
+    GSC is true if the root is not active.
+    """
+
     def satisfied(self, tree: DemeTree) -> bool:
         return not tree.root.is_active
 
     def __str__(self) -> str:
-        return "root_stopped"
+        return "RootStopped"
 
 
-class all_stopped(gsc):
+class AllStopped(GSC):
+    """
+    GSC is true if there are no active demes in the tree.
+    """
+
     def satisfied(self, tree: DemeTree) -> bool:
         return len(list(tree.active_demes)) == 0
 
     def __str__(self) -> str:
-        return "all_stopped"
+        return "AllStopped"
 
 
-class fitness_eval_limit_reached(gsc):
-    def __init__(self, limit: int, weights: Union[List[float], str] = "equal") -> None:
+class WeightingStrategy(StrEnum):
+    EQUAL = "equal"
+    ROOT = "root"
+
+
+class FitnessEvalLimitReached(GSC):
+    """
+    GSC is true if the total number of fitness evaluations in the tree is greater than or equal to the limit.
+    It supports different weighting strategies for the evaluations at different levels of the tree.
+
+    The class can be initialized with a limit and an optional weighting strategy.
+    The weighting strategy determines how evaluations at different levels contribute
+    to the total count. Supported strategies are "equal" and "root", with "equal" being
+    the default. If the strategy is "equal", all levels contribute equally to the total count.
+    If the strategy is "root", only the root level contributes to the total count.
+
+    Args:
+    - limit (int): The threshold number of evaluations to check against.
+    - weights (list[float] | WeightingStrategy): A list of weights corresponding to each level
+    in the tree or a WeightingStrategy specifying a predefined weighting strategy. Defaults to WeightingStrategy.EQUAL.
+    """
+
+    def __init__(
+        self,
+        limit: int,
+        weights: list[float] | WeightingStrategy | None = WeightingStrategy.EQUAL,
+    ) -> None:
         super().__init__()
         self.limit = limit
         self.weights = weights
@@ -60,17 +95,17 @@ class fitness_eval_limit_reached(gsc):
         return n_evals >= self.limit
 
     def _transform_weights(self, n_levels: int):
-        if self.weights == "root":
+        if self.weights == WeightingStrategy.ROOT:
             self.weights = [0 for _ in range(n_levels)]
             self.weights[0] = 1
-        elif self.weights == "equal" or self.weights is None:
+        elif self.weights == WeightingStrategy.EQUAL or self.weights is None:
             self.weights = [1 for _ in range(n_levels)]
 
     def __str__(self) -> str:
-        return f"fitness_eval_limit_reached(limit={self.limit}, weights={self.weights})"
+        return f"FitnessEvalLimitReached(limit={self.limit}, weights={self.weights})"
 
 
-class singular_problem_eval_limit_reached(gsc):
+class SingularProblemEvalLimitReached(GSC):
     def __init__(self, limit: int) -> None:
         super().__init__()
         self.limit = limit
@@ -82,10 +117,10 @@ class singular_problem_eval_limit_reached(gsc):
         return problem.n_evaluations >= self.limit
 
     def __str__(self) -> str:
-        return f"singular_problem_eval_limit_reached(limit={self.limit})"
+        return f"SingularProblemEvalLimitReached(limit={self.limit})"
 
 
-class singular_problem_precision_reached(gsc):
+class SingularProblemPrecisionReached(GSC):
     def __init__(self, problem: PrecisionCutoffProblem):
         super().__init__()
         self.problem = problem
@@ -94,27 +129,25 @@ class singular_problem_precision_reached(gsc):
         return self.problem.hit_precision
 
     def __str__(self) -> str:
-        return f"singular_problem_precision_reached(precision={self.problem.precision})"
+        return f"SingularProblemPrecisionReached(precision={self.problem.precision})"
 
 
-class no_active_nonroot_demes(gsc):
+class NoActiveNonrootDemes(GSC):
     def __init__(self, n_metaepochs: int = 5) -> None:
         super().__init__()
         self.n_metaepochs = n_metaepochs
 
     def satisfied(self, tree: DemeTree) -> bool:
         step = tree.metaepoch_count
-        logger.debug(f"Step {step}")
         for level_no in range(1, tree.height):
             if len(tree.levels[level_no]) == 0:
                 return False
 
             for deme in tree.levels[level_no]:
-                logger.debug(f"Deme {deme.id} st {deme.started_at} mc {deme.metaepoch_count}")
                 if deme.is_active or step <= deme.started_at + deme.metaepoch_count + self.n_metaepochs:
                     return False
 
         return True
 
     def __str__(self) -> str:
-        return f"no_active_nonroot_demes({self.n_metaepochs})"
+        return f"NoActiveNonrootDemes({self.n_metaepochs})"
