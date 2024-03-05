6cd8685a83a94dee1a10b08a083a70ce604838c7
WojtAcht <58685838+WojtAcht@users.noreply.github.com>
2024-03-05 17:23:07 +0100

    feat: use Read The Docs sphinx theme (#24)

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx_search.extension",
 ]
 
 templates_path = ["_templates"]
@@ -27,5 +28,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
