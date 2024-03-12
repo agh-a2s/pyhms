728949902a2a1bf71cba324ad9ff5056e392d132
WojtAcht <wachtelik@gmail.com>
2024-03-12 16:13:57 +0100

    fix: improve doc requirements, add pyhms dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import os
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys
 
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "pyhms"
 copyright = "2024, AGH UST"
