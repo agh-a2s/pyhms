f28dcfd824a88db9613f606fbc322299f34573fb
WojtAcht <wachtelik@gmail.com>
2024-03-12 15:50:41 +0100

    fix: missing problem descriptions

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "pyhms"
 copyright = "2024, AGH UST"
