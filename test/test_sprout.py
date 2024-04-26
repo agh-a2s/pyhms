e5fa07ff758a587397c1b4d7f158877327410108
Hubert-Guzowski <hubert.guzowski@gmail.com>
2024-04-26 18:41:19 +0200

    feat: add new sprout tests
    Additionally removed outdated code and fixed one bug

--- test/test_sprout.py
+++ test/test_sprout.py
@@ -1,206 +1,197 @@
 import unittest
+from copy import deepcopy
 
 import numpy as np
 from pyhms.config import CMALevelConfig, EALevelConfig, TreeConfig
+from pyhms.core.individual import Individual
 from pyhms.core.problem import FunctionProblem
-from pyhms.demes.abstract_deme import compute_centroid
 from pyhms.demes.single_pop_eas.sea import SEA
-from pyhms.sprout.sprout_filters import DemeLimit, LevelLimit, NBC_FarEnough
-from pyhms.sprout.sprout_generators import NBC_Generator
-from pyhms.sprout.sprout_mechanisms import SproutMechanism, get_NBC_sprout, get_simple_sprout
-from pyhms.stop_conditions import DontStop, MetaepochLimit
+from pyhms.sprout.sprout_filters import DemeLimit, FarEnough, LevelLimit, NBC_FarEnough
+from pyhms.sprout.sprout_generators import BestPerDeme, NBC_Generator
+from pyhms.sprout.sprout_mechanisms import SproutMechanism
+from pyhms.stop_conditions import DontStop
 from pyhms.tree import DemeTree
 
-EGG_HOLDER_BOUNDS = np.array([(-512, 512), (-512, 512), (-512, 512)])
+FOUR_FUNNEL_BOUNDS = np.array([(-10.0, 10.0)] * 2)
+
+
+def four_funnels(x) -> float:
+    def funnel(x, center) -> float:
+        return np.sum((x - center) ** 2)
+
+    return np.min(
+        [
+            funnel(x, center)
+            for center in [np.array((-5.0, -5.0)), np.array((5.0, 5.0)), np.array((-5.0, 5.0)), np.array((5.0, -5.0))]
+        ]
+    )
 
 
 class TestSprout(unittest.TestCase):
-    @staticmethod
-    def egg_holder(x) -> float:
-        return sum(
-            [
-                -x[i] * np.sin(np.sqrt(np.abs(x[i] - x[i + 1] - 47)))
-                - (x[i] - 47) * np.sin(np.sqrt(np.abs(0.5 * x[i] + x[i + 1] + 47)))
-                for i in range(len(x) - 2)
-            ]
-        )
+    def setUp(self):
+        genome = [
+            [5.0, 5.0],
+            [5.0, 5.25],
+            [-5.0, -5.5],
+            [-7.438652231484955, 1.6694449410425545],
+            [8.868488721370134, -3.8092004083209963],
+            [-2.3623991956204105, 3.540653675138808],
+            [4.46, -4.49],
+            [-2.5619700848072258, 2.240573438147379],
+            [2.4633250549304986, 5.347669720933412],
+            [-7.703343438597178, -4.0488667762712645],
+            [0.9313567871638302, 6.166115779099393],
+            [2.415406130753734, -6.885780821947341],
+            [5.749793158123332, 6.091968174818984],
+            [9.868256923059192, -0.856544263545203],
+            [8.461437828094471, -7.69756109622826],
+            [0.4762125269013602, -2.5571632669301643],
+            [2.891842394903989, -5.41814403371089],
+            [2.285558012997315, 3.403792214048396],
+            [-6.39, 2.88],
+            [-9.177658322165303, -0.3086626198612663],
+        ]
+        self.minimize_problem = FunctionProblem(lambda x: four_funnels(x), bounds=FOUR_FUNNEL_BOUNDS, maximize=False)
+        self.maximize_problem = FunctionProblem(lambda x: -four_funnels(x), bounds=FOUR_FUNNEL_BOUNDS, maximize=True)
+        self.initial_population_min = [
+            Individual(np.array(genome[i]), self.minimize_problem, self.minimize_problem.evaluate(genome[i]))
+            for i in range(len(genome))
+        ]
+        self.initial_population_max = [
+            Individual(np.array(genome[i]), self.maximize_problem, self.maximize_problem.evaluate(genome[i]))
+            for i in range(len(genome))
+        ]
 
-    def test_simple_sprout(self):
-        for limit in [2, 4, 8]:
-            correct_sprout = True
-            function_problem = FunctionProblem(lambda x: self.egg_holder(x), bounds=EGG_HOLDER_BOUNDS, maximize=False)
-            gsc = MetaepochLimit(limit=20)
-            sprout_cond = get_simple_sprout(10.0, limit)
-
-            config = [
-                EALevelConfig(
-                    ea_class=SEA,
-                    generations=2,
-                    problem=function_problem,
-                    pop_size=20,
-                    mutation_std=50.0,
-                    lsc=DontStop(),
-                ),
-                CMALevelConfig(
-                    generations=2,
-                    problem=function_problem,
-                    sigma0=2.5,
-                    lsc=MetaepochLimit(limit=8),
-                ),
-            ]
-
-            config = TreeConfig(config, gsc, sprout_cond)
-            hms_tree = DemeTree(config)
-            while not hms_tree._gsc(hms_tree):
-                hms_tree.metaepoch_count += 1
-                hms_tree.run_metaepoch()
-                if not hms_tree._gsc(hms_tree):
-                    hms_tree.run_sprout()
-
-                if not all(
-                    [len(list(filter(lambda deme: deme.is_active, level))) <= limit for level in hms_tree.levels]
-                ):
-                    correct_sprout = False
-                    print(f"\nFailed level limit sprout test. Limit: {limit}")
-                    print("Deme info:")
-                    for level, deme in hms_tree.all_demes:
-                        print(f"Level {level}")
-                        print(f"Active({deme.is_active}) {deme}")
-                        print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                        print(f"Current centroid position {compute_centroid(deme.current_population)}")
-                    break
-
-                if hms_tree.metaepoch_count == 20:
-                    print(f"\nSuccesful level limit sprout test. Limit: {limit}")
-                    print("Deme info:")
-                    for level, deme in hms_tree.all_demes:
-                        print(f"Level {level}")
-                        print(f"Active({deme.is_active}) {deme}")
-                        print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                        print(f"Current centroid position {compute_centroid(deme.current_population)}")
-
-            self.assertEqual(
-                correct_sprout,
-                True,
-                "Should be no more than 2 active demes on each level",
-            )
-
-    def test_default_nbc_sprout(self):
-        correct_sprout = True
-        function_problem = FunctionProblem(lambda x: self.egg_holder(x), maximize=False, bounds=EGG_HOLDER_BOUNDS)
-        gsc = MetaepochLimit(limit=20)
-        sprout_cond = get_NBC_sprout()
-        limit = 4
-
-        config = [
+        minimize_levels = [
             EALevelConfig(
                 ea_class=SEA,
-                generations=2,
-                problem=function_problem,
+                generations=1,
+                problem=self.minimize_problem,
                 pop_size=20,
-                mutation_std=50.0,
+                mutation_std=1.0,
                 lsc=DontStop(),
             ),
             CMALevelConfig(
-                generations=2,
-                problem=function_problem,
-                sigma0=2.5,
-                lsc=MetaepochLimit(limit=8),
+                generations=4,
+                problem=self.minimize_problem,
+                sigma0=0.01,
+                lsc=DontStop(),
             ),
         ]
+        maximize_levels = deepcopy(minimize_levels)
+        maximize_levels[0].problem = self.maximize_problem
+        maximize_levels[1].problem = self.maximize_problem
+        self.minimize_config = TreeConfig(minimize_levels, DontStop(), SproutMechanism([], [], []), options={})
+        self.maximize_config = TreeConfig(maximize_levels, DontStop(), SproutMechanism([], [], []), options={})
 
-        config = TreeConfig(config, gsc, sprout_cond)
-        hms_tree = DemeTree(config)
-        while not hms_tree._gsc(hms_tree):
-            hms_tree.metaepoch_count += 1
-            hms_tree.run_metaepoch()
-            if not hms_tree._gsc(hms_tree):
-                hms_tree.run_sprout()
+    def test_simple_sprout_candidates(self):
+        tree = DemeTree(self.minimize_config)
+        sprout = SproutMechanism(BestPerDeme(), [FarEnough(1.0, 2)], [LevelLimit(2)])
 
-            if not all([len(list(filter(lambda deme: deme.is_active, level))) <= limit for level in hms_tree.levels]):
-                correct_sprout = False
-                print(f"\nFailed default NBC sprout test. Limit: {limit}")
-                print("Deme info:")
-                for level, deme in hms_tree.all_demes:
-                    print(f"Level {level}")
-                    print(f"Active({deme.is_active}) {deme}")
-                    print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                    print(f"Current centroid position {compute_centroid(deme.current_population)}")
-                break
+        tree._sprout_mechanism = sprout
+        tree.root._history = [[self.initial_population_min]]
+        first_generated = sprout.candidates_generator(tree)
+        self.assertCountEqual(first_generated[tree.root].individuals[0].genome, self.initial_population_min[0].genome)
+        first_candidates = sprout.apply_deme_filters(first_generated, tree)
+        first_candidates = sprout.apply_tree_filters(first_candidates, tree)
+        self.assertCountEqual(first_candidates[tree.root].individuals[0].genome, self.initial_population_min[0].genome)
 
-            if hms_tree.metaepoch_count == 19:
-                print(f"\nSuccesful default NBC sprout test. Limit: {limit}")
-                print("Deme info:")
-                for level, deme in hms_tree.all_demes:
-                    print(f"Level {level}")
-                    print(f"Active({deme.is_active}) {deme}")
-                    print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                    print(f"Current centroid position {compute_centroid(deme.current_population)}")
+        tree._do_sprout(first_candidates)
+        second_generated = sprout.candidates_generator(tree)
+        second_candidates = sprout.apply_deme_filters(second_generated, tree)
+        self.assertEqual(len(second_candidates[tree.root].individuals), 0)
 
-            self.assertEqual(
-                correct_sprout,
-                True,
-                "Should be no more than 2 active demes on each level",
-            )
-
-    def test_nbc_sprout_with_truncation(self):
-        correct_sprout = True
-        function_problem = FunctionProblem(lambda x: self.egg_holder(x), maximize=False, bounds=EGG_HOLDER_BOUNDS)
-        gsc = MetaepochLimit(limit=20)
-        sprout_cond = SproutMechanism(
-            NBC_Generator(2.0, 0.4),
+    def test_nbc_sprout_candidates(self):
+        tree = DemeTree(self.minimize_config)
+        sprout = SproutMechanism(
+            NBC_Generator(2.0, 0.8),
             [NBC_FarEnough(2.0, 2), DemeLimit(1)],
-            [LevelLimit(4)],
+            [LevelLimit(2)],
         )
-        limit = 4
 
-        config = [
-            EALevelConfig(
-                ea_class=SEA,
-                generations=2,
-                problem=function_problem,
-                pop_size=20,
-                mutation_std=50.0,
-                lsc=DontStop(),
-            ),
-            CMALevelConfig(
-                generations=2,
-                problem=function_problem,
-                sigma0=2.5,
-                lsc=MetaepochLimit(limit=8),
-            ),
-        ]
+        tree._sprout_mechanism = sprout
+        tree.root._history = [[self.initial_population_min]]
+        first_generated = sprout.candidates_generator(tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in first_generated[tree.root].individuals]).flatten(),
+            np.array([self.initial_population_min[i].genome for i in [0, 2, 6, 18]]).flatten(),
+        )
+        first_candidates = sprout.apply_deme_filters(first_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in first_candidates[tree.root].individuals]).flatten(),
+            self.initial_population_min[0].genome,
+        )
 
-        config = TreeConfig(config, gsc, sprout_cond)
-        hms_tree = DemeTree(config)
-        while not hms_tree._gsc(hms_tree):
-            hms_tree.metaepoch_count += 1
-            hms_tree.run_metaepoch()
-            if not hms_tree._gsc(hms_tree):
-                hms_tree.run_sprout()
+        tree._do_sprout(first_candidates)
+        second_generated = sprout.candidates_generator(tree)
+        second_candidates = sprout.apply_deme_filters(second_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in second_generated[tree.root].individuals]).flatten(),
+            self.initial_population_min[2].genome,
+        )
 
-            if not all([len(list(filter(lambda deme: deme.is_active, level))) <= limit for level in hms_tree.levels]):
-                correct_sprout = False
-                print(f"\nFailed truncation NBC sprout test. Limit: {limit}")
-                print("Deme info:")
-                for level, deme in hms_tree.all_demes:
-                    print(f"Level {level}")
-                    print(f"Active({deme.is_active}) {deme}")
-                    print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                    print(f"Current centroid position {compute_centroid(deme.current_population)}")
-                break
+        tree._do_sprout(second_candidates)
+        third_generated = sprout.candidates_generator(tree)
+        third_candidates = sprout.apply_deme_filters(third_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in third_candidates[tree.root].individuals]).flatten(),
+            self.initial_population_min[6].genome,
+        )
+        third_candidates = sprout.apply_tree_filters(third_generated, tree)
+        self.assertEqual(len(third_candidates[tree.root].individuals), 0)
 
-            if hms_tree.metaepoch_count == 19:
-                print(f"\nSuccesful truncation NBC sprout test. Limit: {limit}")
-                print("Deme info:")
-                for level, deme in hms_tree.all_demes:
-                    print(f"Level {level}")
-                    print(f"Active({deme.is_active}) {deme}")
-                    print(f"Centroid position at the start {compute_centroid(deme.history[0])}")
-                    print(f"Current centroid position {compute_centroid(deme.current_population)}")
+    def test_simple_sprout_candidates_maximization(self):
+        tree = DemeTree(self.maximize_config)
+        sprout = SproutMechanism(BestPerDeme(), [FarEnough(1.0, 2)], [LevelLimit(2)])
 
-            self.assertEqual(
-                correct_sprout,
-                True,
-                "Should be no more than 2 active demes on each level",
-            )
+        tree._sprout_mechanism = sprout
+        tree.root._history = [[self.initial_population_max]]
+        first_generated = sprout.candidates_generator(tree)
+        self.assertCountEqual(first_generated[tree.root].individuals[0].genome, self.initial_population_max[0].genome)
+        first_candidates = sprout.apply_deme_filters(first_generated, tree)
+        first_candidates = sprout.apply_tree_filters(first_candidates, tree)
+        self.assertCountEqual(first_candidates[tree.root].individuals[0].genome, self.initial_population_max[0].genome)
+
+        tree._do_sprout(first_candidates)
+        second_generated = sprout.candidates_generator(tree)
+        second_candidates = sprout.apply_deme_filters(second_generated, tree)
+        self.assertEqual(len(second_candidates[tree.root].individuals), 0)
+
+    def test_nbc_sprout_candidates_maximization(self):
+        tree = DemeTree(self.maximize_config)
+        sprout = SproutMechanism(
+            NBC_Generator(2.0, 0.8),
+            [NBC_FarEnough(2.0, 2), DemeLimit(1)],
+            [LevelLimit(2)],
+        )
+
+        tree._sprout_mechanism = sprout
+        tree.root._history = [[self.initial_population_max]]
+        first_generated = sprout.candidates_generator(tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in first_generated[tree.root].individuals]).flatten(),
+            np.array([self.initial_population_max[i].genome for i in [0, 2, 6, 18]]).flatten(),
+        )
+        first_candidates = sprout.apply_deme_filters(first_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in first_candidates[tree.root].individuals]).flatten(),
+            self.initial_population_max[0].genome,
+        )
+
+        tree._do_sprout(first_candidates)
+        second_generated = sprout.candidates_generator(tree)
+        second_candidates = sprout.apply_deme_filters(second_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in second_generated[tree.root].individuals]).flatten(),
+            self.initial_population_max[2].genome,
+        )
+
+        tree._do_sprout(second_candidates)
+        third_generated = sprout.candidates_generator(tree)
+        third_candidates = sprout.apply_deme_filters(third_generated, tree)
+        self.assertCountEqual(
+            np.array([ind.genome for ind in third_candidates[tree.root].individuals]).flatten(),
+            self.initial_population_max[6].genome,
+        )
+        third_candidates = sprout.apply_tree_filters(third_generated, tree)
+        self.assertEqual(len(third_candidates[tree.root].individuals), 0)
