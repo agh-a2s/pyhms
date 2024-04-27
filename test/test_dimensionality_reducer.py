ed7844b272d35c6782ebe09cbd56b477d875b354
WojtAcht <wachtelik@gmail.com>
2024-04-27 19:58:19 +0200

    fix: improve test after changing DimensionalityReducer

--- test/test_dimensionality_reducer.py
+++ test/test_dimensionality_reducer.py
@@ -8,6 +8,7 @@ class TestNaiveDimensionalityReducer(unittest.TestCase):
     def test_fit_transform(self):
         X = np.random.rand(1000, 4)
         reducer = NaiveDimensionalityReducer()
-        X_reduced = reducer.fit_transform(X)
+        reducer.fit(X)
+        X_reduced = reducer.transform(X)
         self.assertEqual(X_reduced.shape, (1000, 2))
         self.assertTrue(np.array_equal(X_reduced, X[:, :2]))
