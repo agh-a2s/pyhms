e65605f44676daaea9035f61890c3087275ef150
WojtAcht <wachtelik@gmail.com>
2024-05-11 12:57:06 +0200

    feat: improve SHADE implementation

--- pyhms/demes/single_pop_eas/de.py
+++ pyhms/demes/single_pop_eas/de.py
@@ -43,13 +43,14 @@ class BinaryMutationWithDither(VariationalOperator):
         return Population(new_genomes, new_fitness, population.problem)
 
 
-class CurrentToPBestMutation(VariationalOperator):
-    def __init__(self, f: np.ndarray | None, p: np.ndarray):
-        self.f = f
-        self.p = p
-
-    def __call__(self, population: Population) -> Population:
-        assert self.f is not None
+class CurrentToPBestMutation:
+    def __call__(
+        self,
+        population: Population,
+        archive: Population,
+        f: np.ndarray,
+        p: np.ndarray,
+    ) -> Population:
         if population.size < 4:
             return population
         sorted_fitness_indexes = (
@@ -58,15 +59,17 @@ class CurrentToPBestMutation(VariationalOperator):
             else np.argsort(-1 * population.fitnesses)
         )
         p_best = []
-        for p_i in self.p:
+        for p_i in p:
             best_index = sorted_fitness_indexes[: max(2, int(round(p_i * population.size)))]
             p_best.append(np.random.choice(best_index))
         p_best_np = np.array(p_best)
         randoms = get_randoms(population)
+        randoms_with_archive = get_randoms(population.merge(archive)) if archive is not None else randoms
+        randoms_with_archive = randoms_with_archive[: len(randoms)]
         mutated_genomes = (
             population.genomes
-            + self.f * (population.genomes[p_best_np] - population.genomes)
-            + self.f * (randoms[:, 0] - randoms[:, 1])
+            + f * (population.genomes[p_best_np] - population.genomes)
+            + f * (randoms[:, 0] - randoms_with_archive[:, 1])
         )
         new_genomes = apply_bounds(mutated_genomes, population.problem.bounds, "reflect")
         new_fitness = np.where(
@@ -76,20 +79,18 @@ class CurrentToPBestMutation(VariationalOperator):
         )
         return Population(new_genomes, new_fitness, population.problem)
 
-    def adapt(self, f: np.ndarray) -> None:
-        self.f = f
-
 
 class Crossover:
-    def __init__(self, probability: np.ndarray | None) -> None:
-        self.probability = probability
-
-    def __call__(self, population: Population, mutated_population: Population) -> Population:
-        assert self.probability is not None
+    def __call__(
+        self,
+        population: Population,
+        mutated_population: Population,
+        probability: np.ndarray | float,
+    ) -> Population:
         chosen = np.random.rand(*population.genomes.shape)
         j_rand = np.random.randint(0, population.size)
         chosen[j_rand :: population.size] = 0  # noqa: E203
-        new_genomes = np.where(chosen <= self.probability, mutated_population.genomes, population.genomes)
+        new_genomes = np.where(chosen <= probability, mutated_population.genomes, population.genomes)
         new_fitness = np.where(
             np.all(new_genomes == population.genomes, axis=1),
             population.fitnesses,
@@ -98,20 +99,18 @@ class Crossover:
         new_population = Population(new_genomes, new_fitness, population.problem)
         return new_population
 
-    def adapt(self, probability: np.ndarray) -> None:
-        self.probability = probability
-
 
 class DE:
     def __init__(self, use_dither: bool, crossover_probability: float, f: float | None = None) -> None:
+        self._crossover_probability = crossover_probability
         self._mutation = BinaryMutationWithDither() if use_dither else BinaryMutation(f=f)
-        self._crossover = Crossover(crossover_probability)
+        self._crossover = Crossover()
 
     def run(self, parents: list[Individual]) -> list[Individual]:
         parent_population = Population.from_individuals(parents)
         trial_population = parent_population.copy()
         trial_population = self._mutation(trial_population)
-        trial_population = self._crossover(parent_population, trial_population)
+        trial_population = self._crossover(parent_population, trial_population, self._crossover_probability)
         trial_population.evaluate()
         new_population_indices = (
             (trial_population.fitnesses >= parent_population.fitnesses)
@@ -166,23 +165,19 @@ class SHADE:
         self._memory_size = memory_size
         self._m_cr = np.ones(memory_size) * 0.5
         self._m_f = np.ones(memory_size) * 0.5
-        self._archive = []
+        self._archive: Population | None = None
         self._all_indexes = list(range(memory_size))
         self._pop_size = population_size
-        self._init_pop_size = population_size
         self._k = 0
-        self._p = np.ones(population_size) * 0.11
-        self._mutation = CurrentToPBestMutation(f=None, p=self._p)
-        self._crossover = Crossover(None)
+        self._mutation = CurrentToPBestMutation()
+        self._crossover = Crossover()
 
     def run(self, parents: list[Individual]) -> list[Individual]:
         parent_population = Population.from_individuals(parents)
         offspring_population = parent_population.copy()
-        cr, f = self._get_params()
-        self._mutation.adapt(f.reshape(len(f), 1))
-        self._crossover.adapt(cr.reshape(len(f), 1))
-        offspring_population = self._mutation(offspring_population)
-        offspring_population = self._crossover(parent_population, offspring_population)
+        cr, f, p = self._get_params()
+        offspring_population = self._mutation(offspring_population, self._archive, f.reshape(len(f), 1), p)
+        offspring_population = self._crossover(parent_population, offspring_population, cr.reshape(len(f), 1))
         offspring_population.evaluate()
         new_population_indices = (
             (offspring_population.fitnesses >= parent_population.fitnesses)
@@ -190,6 +185,10 @@ class SHADE:
             else (offspring_population.fitnesses <= parent_population.fitnesses)
         )
         new_population = offspring_population[new_population_indices].merge(parent_population[~new_population_indices])
+        if self._archive is None:
+            self._archive = parent_population[new_population_indices]
+        else:
+            self._archive = self._archive.merge(parent_population[new_population_indices])
         if new_population_indices.any():
             self._update_memory(
                 cr,
@@ -198,10 +197,9 @@ class SHADE:
                 offspring_population.fitnesses,
                 new_population_indices,
             )
-        self._archive.extend(new_population.genomes[new_population_indices])
         return new_population.to_individuals()
 
-    def _get_params(self) -> tuple[np.ndarray, np.ndarray]:
+    def _get_params(self) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         r = np.random.choice(self._all_indexes, self._pop_size)
         cr = np.random.normal(self._m_cr[r], 0.1, self._pop_size)
         cr = np.clip(cr, 0, 1)
@@ -211,7 +209,8 @@ class SHADE:
         while sum(f <= 0) != 0:
             r = np.random.choice(self._all_indexes, sum(f <= 0))
             f[f <= 0] = scipy.stats.cauchy.rvs(loc=self._m_f[r], scale=0.1, size=sum(f <= 0))
-        return cr, f
+        p = np.random.uniform(low=2 / self._pop_size, high=0.2, size=self._pop_size)
+        return cr, f, p
 
     def _update_memory(
         self,
@@ -221,12 +220,18 @@ class SHADE:
         c_fitness: np.ndarray,
         indexes: np.ndarray,
     ):
-        weights = np.abs(fitness[indexes] - c_fitness[indexes])
-        weights /= np.sum(weights)
-        self._m_cr[self._k] = np.sum(weights * cr[indexes] ** 2) / np.sum(weights * cr[indexes])
-        if np.isnan(self._m_cr[self._k]):
+        if self._archive.size > self._memory_size:
+            random_indices = np.random.choice(np.arange(self._archive.size), self._memory_size, replace=False)
+            self._archive = self._archive[random_indices]
+        if max(cr) != 0:
+            weights = np.abs(fitness[indexes] - c_fitness[indexes])
+            weights /= np.sum(weights)
+            self._m_cr[self._k] = np.sum(weights * cr[indexes])
+        else:
             self._m_cr[self._k] = 1
-        self._m_f[self._k] = np.sum(weights * f[indexes] ** 2) / np.sum(weights * f[indexes])
+
+        self._m_f[self._k] = np.sum(f[indexes] ** 2) / np.sum(f[indexes])
+
         self._k += 1
         if self._k == self._memory_size:
             self._k = 0
