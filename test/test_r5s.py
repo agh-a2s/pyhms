9326809e45f0d3739a7690de5807cef96b4841a0
WojtAcht <wachtelik@gmail.com>
2024-09-08 12:07:17 +0200

    fix: run pre commit

--- test/test_r5s.py
+++ test/test_r5s.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 from pyhms.core.individual import Individual
 from pyhms.utils.r5s import R5SSelection
+
 from .config import SQUARE_PROBLEM
 
 
@@ -24,9 +25,7 @@ class TestR5S(unittest.TestCase):
                 [-0.15, 0.85],
             ]
         )
-        fitness_values = np.array(
-            [0.1, 0.5, 0.9, 0.3, 0.6, 0.8, 0.4, 0.95, 0.25, 0.7, 0.55, 0.45]
-        )
+        fitness_values = np.array([0.1, 0.5, 0.9, 0.3, 0.6, 0.8, 0.4, 0.95, 0.25, 0.7, 0.55, 0.45])
         individuals = [
             # The fitness values are not real, they are just for testing purposes.
             # R5SSelection doesn't use the problem to evaluate the individuals.
