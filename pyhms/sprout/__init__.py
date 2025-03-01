9e62d69950df5cb514da822078e8efa68fc1752c
Wojciech Achtelik <58685838+WojtAcht@users.noreply.github.com>
2025-03-01 12:11:59 +0100

    feat: add new MahalanobisFarEnough sprout filter (#87)

--- pyhms/sprout/__init__.py
+++ pyhms/sprout/__init__.py
@@ -1,3 +1,3 @@
-from .sprout_filters import DemeLimit, FarEnough, LevelLimit, NBC_FarEnough, SkipSameSprout
+from .sprout_filters import DemeLimit, FarEnough, LevelLimit, MahalanobisFarEnough, NBC_FarEnough, SkipSameSprout
 from .sprout_generators import BestPerDeme, NBC_Generator, NBCGeneratorWithLocalMethod
 from .sprout_mechanisms import SproutMechanism, get_NBC_sprout, get_simple_sprout
